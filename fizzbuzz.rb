d7aacad6a84e8be85e67ec972952027ecd4814f2
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-06-20 21:33:49 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(number)
-  print "fuzz" if number % 3
+def fizzbuzz(number
+  string = nil
+  string = "fuzz" if number % 3
   print "buzz" if number % 5
   print "\n"
 end
