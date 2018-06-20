66534c8eed53e1771715c703c7157d65e84a385c
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-06-20 21:36:48 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
+  puts number
   string = ""
   string += "Fizz" if number % 3 == 0
+  puts number
   string += "Buzz" if number % 5 == 0
+  puts number
 end
+
